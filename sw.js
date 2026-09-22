f6da23191424c70dccaf21fda155695640198f97
1532509551 <1532509551@qq.com>
2026-09-22 11:08:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
-const CACHE='cell-density-v2';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='cell-density-v4';
+
+const ASSETS=[
+ './',
+ './index.html',
+ './style.css',
+ './app.js',
+ './manifest.webmanifest',
+ './icon.svg',
+ './sw.js'
+];
+
+self.addEventListener('install',event=>{
+ event.waitUntil(
+   caches.open(CACHE)
+   .then(cache=>cache.addAll(ASSETS))
+   .then(()=>self.skipWaiting())
+ );
+});
+
+self.addEventListener('activate',event=>{
+ event.waitUntil(
+   caches.keys()
+   .then(keys=>
+     Promise.all(
+       keys
+       .filter(key=>key!==CACHE)
+       .map(key=>caches.delete(key))
+     )
+   )
+   .then(()=>self.clients.claim())
+ );
+});
+
+self.addEventListener('fetch',event=>{
+ event.respondWith(
+   caches.match(event.request)
+   .then(response=>response || fetch(event.request))
+ );
+});
